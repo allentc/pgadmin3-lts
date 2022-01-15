08bedaf118e2c50e377ce9ebbbb677894bef1cbd
Sebastian Dietrich <SebDieBln@users.noreply.github.com>
2022-01-15 20:17:57 +0100

    Make the installer use the C++-runtime merge-module for the current toolset
    Note that this unfortunately requires a downgrade from VS2022 to VS2019, because the merge modules for toolset v142 are not available in VS2022.

--- pkg/win32/Make.bat
+++ pkg/win32/Make.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 REM
 REM Setup the following values as required for the installation
@@ -49,12 +49,12 @@ GOTO EXIT
 
 :BUILD_PACKAGE
 
-if "%2"=="" GOTO ERR_USAGE
+if (%3)==() GOTO ERR_USAGE
 
 echo.
 echo Building %APPNAME% Installer...
 
-"%WIX%\bin\candle" -nologo -dWXDIR="%WXWIN%" -dPLATFORM_TOOLSET_VERSION=%2 -dPGDIR="%PGDIR%" -dBUILDTREE="%BUILDTREE%" -dBRANDED=%BRANDED% -dBRANDINGDIR="%BRANDINGDIR%" -dAPPVENDOR="%APPVENDOR%" -dAPPNAME="%APPNAME%" -dAPPKEYWORDS="%APPKEYWORDS%" -dAPPCOMMENTS="%APPCOMMENTS%" -dAPPDESCRIPTION="%APPDESCRIPTION%" -dAPPVERSION="%1" -dSYSTEM32DIR="%SystemRoot%\System32" -dPFILESDIR="%ProgramFiles%" src/pgadmin3.wxs
+"%WIX%\bin\candle" -nologo -dWXDIR="%WXWIN%" -dPLATFORM_TOOLSET_VERSION=%2 -dVC_TOOLS_REDIST_INSTALL_DIR=%3 -dPGDIR="%PGDIR%" -dBUILDTREE="%BUILDTREE%" -dBRANDED=%BRANDED% -dBRANDINGDIR="%BRANDINGDIR%" -dAPPVENDOR="%APPVENDOR%" -dAPPNAME="%APPNAME%" -dAPPKEYWORDS="%APPKEYWORDS%" -dAPPCOMMENTS="%APPCOMMENTS%" -dAPPDESCRIPTION="%APPDESCRIPTION%" -dAPPVERSION="%1" -dSYSTEM32DIR="%SystemRoot%\System32" -dPFILESDIR="%ProgramFiles%" src/pgadmin3.wxs
 IF ERRORLEVEL 1 GOTO ERR_HANDLER
 
 "%WIX%\bin\light" -sice:ICE03 -sice:ICE25 -sice:ICE82 -sw1101 -nologo -ext WixUIExtension -cultures:en-us pgadmin3.wixobj
@@ -71,7 +71,7 @@ GOTO EXIT
 
 :ERR_USAGE
 echo Invalid command line options.
-echo Usage: "Make.bat <Major.Minor version number> <Platform toolset version>"
+echo Usage: "Make.bat <Major.Minor version number> <Platform toolset version> <VCToolsRedistInstallDir>"
 echo        "Make.bat REGENGUIDS"
 echo.
 GOTO EXIT
